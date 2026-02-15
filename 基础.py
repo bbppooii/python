46360f7bb06ae087f16b87f6d42ff2f08145843d
bbppooi <147604819+bbppooii@users.noreply.github.com>
2026-02-16 00:19:33 +0800

    Add files via upload

---
+++ 基础.py
@@ -0,0 +1,9 @@
+# class Test03():
+#     def test03_01(self):
+#         print('test01')
+#
+#     def test03_02(self):
+#         print('test02')
+#
+#     def test03_03(self):
+#         print('test02')
