9d438022c2532620d99659f796321c294cb71f6d
bbppooi <147604819+bbppooii@users.noreply.github.com>
2026-02-20 22:53:00 +0800

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,11 @@
+import pytest
+# @pytest.fixture(scope="module")
+# def din():
+#     print("qian")
+#     yield
+#     print("hou")
+# @pytest.fixture(scope="session",autouse="ture")
+# def din():
+#     print("qian")
+#     yield
+#     print("hou")
