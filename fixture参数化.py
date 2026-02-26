4ce1ce0e7206e076877524358b1157a1e464d789
bbppooi <147604819+bbppooii@users.noreply.github.com>
2026-02-26 13:47:41 +0800

    Add files via upload

---
+++ fixture参数化.py
@@ -0,0 +1,12 @@
+import pytest
+import requests
+# class Test02:
+#     def test01(self,din):
+#         print(1)
+#     def test02(self,din):
+#         print(2)
+# class Test02:
+#     def test01(self):
+#         print(1)
+#     def test02(self):
+#         print(2)
